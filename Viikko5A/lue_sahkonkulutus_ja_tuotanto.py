cdbd0921ec9440d32bc923593ae06a34012fd39e
Henna Linnala <henna.linnala@centria.fi>
2025-12-05 18:38:47 +0200

    osaispalyhentaa

--- Viikko5A/lue_sahkonkulutus_ja_tuotanto.py
+++ Viikko5A/lue_sahkonkulutus_ja_tuotanto.py
@@ -4,6 +4,7 @@ from datetime import datetime, date
 from typing import List, Dict
 
 def muunna_tiedot(sahko: list) -> list:
+    """Muuttaa tietojen tietotyyppiä"""
     muutettu_tieto = []
     muutettu_tieto.append(datetime.fromisoformat(sahko[0]))
     muutettu_tieto.append(int(sahko[1]))
@@ -15,52 +16,60 @@ def muunna_tiedot(sahko: list) -> list:
     return muutettu_tieto
 
 def sahkonkulutus_ja_tuotanto(data: str) -> list:
-# """Lukee CSV-tiedoston ja palauttaa rivit sopivassa rakenteessa."""
+    """Lukee CSV-tiedoston ja palauttaa rivit"""
+    """Next(f) poistaa esittelytiedon"""
     sahkodata = []
     with open(data, "r", encoding="utf-8") as f:
-        next(f) #Ottaa sarakeiden esittelytiteo pois
+        next(f) 
         for sahko in f:
             sahko = sahko.strip()
             sahkon_tiedot = sahko.split(';')
             sahkodata.append(muunna_tiedot(sahkon_tiedot))
     return sahkodata
 
-#def hae_paiva(sahkodata: list[str]) -> datetime:
-    #paivamaara = datetime.strptime(sahkodata[0], "%Y-%m-%d").date()
-    #suomalainenpvm = paivamaara.strftime("%d.%m.%Y")
-    #return suomalainenpvm
 
-def paivittainen_data(paiva: date, sahkodata: list) -> list:
+from datetime import date
+
+def paivittainen_data(paiva: str, sahkodata: list) -> int:
+    vuorokausi = int(paiva.split(".")[0])
+    kuukausi = int(paiva.split(".")[1])
+    vuosi = int(paiva.split(".")[2])
+    tiedot = []
     kulutus = [0, 0, 0]
     tuotanto = [0, 0, 0]
     for data in sahkodata:
-        if data[0].date() == paiva:
-            kulutus[0] +- data[1]
-            kulutus[1] +- data[2]
+        if data[0].date() == date(vuosi, kuukausi, vuorokausi):
+            kulutus[0] += data[1] 
+            kulutus[1] += data[2] 
+            kulutus[2] += data[3] 
+            tuotanto[0] += data[4] 
+            tuotanto[1] += data [5]
+            tuotanto[2] += data[6] 
 
+    tiedot.append(kulutus[0]/1000)
+    tiedot.append(kulutus[1]/1000)
+    tiedot.append(kulutus[2]/1000)
+    tiedot.append(tuotanto[0]/1000)
+    tiedot.append(tuotanto[1]/1000)
+    tiedot.append(tuotanto[2]/1000)
+    return tiedot
 
-#def muutos(data: list):
-    #/1000
-    #kulutusdata/1000
-    #kulutusdata/1000
-    #tuotanto/1000
-    #tuotanto/1000
-    #tuotanto/1000
 
 def main():
+    """"Pääohjelma, jonka tehtävänä on lukea data, laskea yhteenvedot ja tulostaa raportti ellen jotenkin muuta koko hommaa"""
     sahkodata = sahkonkulutus_ja_tuotanto("viikko42.csv")
-    """Ohjelman pääfunktio: lukee datan, laskee yhteenvedot ja tulostaa raportin."""
     print("\n\tViikon 42 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", end="\n\n")
     print("Päivä\t\t    Pvm\t\t   Kulutus [kWh]\t Tuotanto [kWh]")
-    print("\t\t(pv.kk.vvvv)\tv1\tv2\tv3  \tv1\tv2\tv3")
+    print("\t\t(pv.kk.vvvv) \tv1\tv2\tv3  \tv1\tv2\tv3")
     print("---------------------------------------------------------------------------")
-    print("Maanantai\t", sahkodata[0][0].strftime("%d.%m.%Y"),)
-    print("Tiistai\t", )
-    print("Keskiviikko\t",)
-    print("Torstai\t",)
-    print("Perjantai\t",)
-    print("Lauantai\t",)
-    print("Sunnuntai\t",)
+    maanantain_data = paivittainen_data("13.10.2025", sahkodata)
+    print("Maanantai\t" + "13.10.2025\t",f"{maanantain_data[0]:.2f}".replace('.', ','), end= "\t")
+    print(f"{maanantain_data[1]:.2f}".replace('.', ','), end= "\t")
+    print(f"{maanantain_data[2]:.2f}".replace('.', ','), end= "\t")
+    print(f"{maanantain_data[3]:.2f}".replace('.', ','), end= "\t")
+    print(f"{maanantain_data[4]:.2f}".replace('.', ','), end= "\t")
+    print(f"{maanantain_data[5]:.2f}".replace('.', ','))
+
 
 if __name__ == "__main__":
     main()
--- Viikko5B/raportti_tiedostoon.py
+++ Viikko5B/raportti_tiedostoon.py
@@ -1,8 +1,13 @@
 #def main():
-#    kulutus_tuotanto_viikko41 = lue_data("viikko41.csv")
+# kulutus_tuotanto_viikko41 = lue_data("viikko41.csv")
+# kulutus_tuotanto_viikko42 = lue_data("viikko42.csv")
+# kulutus_tuotanto_viikko43 = lue_data("viikko43.csv")
 
 #with open("yhteenveto.txt", "w", encoding="utf-8") as f:
-#    f.write("\nHei maailma\n")
+#    f.write(viikko41)
+#    f.write(viikko42)
+#    f.write(viikko43)
+
 
 #if __name__ == "__main__":
 #    main()
